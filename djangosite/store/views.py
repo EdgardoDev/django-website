614794a3bcd1003366ffa181769e46dab3874f38
EdgardoDev <ep-marketing-media@live.com>
2022-02-21 10:34:35 +0100

    Added View decorators and Pagination

--- djangosite/store/views.py
+++ djangosite/store/views.py
@@ -1,7 +1,18 @@
+from django.views import View
+from django.core.exceptions import ViewDoesNotExist
+from django.core.paginator import Paginator, PageNotAnInteger
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib import messages
+from django.urls import path
+from django.views.decorators.cache import cache_control, cache_page
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+from django.http import HttpResponse, HttpResponseNotFound, request, response
+from django.views.decorators.gzip import gzip_page
+from django.views.decorators.http import require_http_methods
+from django.views.generic import ListView, TemplateView
 
 def index(request):
     return HttpResponse("Hello, this is the index view")
@@ -9,5 +20,19 @@ def index(request):
 def detail(request):
     return HttpResponse("This is the detail view")
 
+@csrf_exempt
+@cache_page(900)
+@require_http_methods(["GET"])
 def electronics(request):
-    return HttpResponse("This is the electronics view")
+    items = ("Windows PC", "Apple Mac", "Apple IPhone", "Lenovo", "Samsung", "Google" )
+    
+    if request.method == 'GET':
+        paginator = Paginator(items, 2)
+        pages = request.GET.get('page', 1)
+        try:
+            items = paginator.page(pages)
+        except PageNotAnInteger:
+            items = paginator.page(1)
+        return render(request, "store/list.html", {"items": items})
+    elif request.method == 'POST':
+        return HttpResponseNotFound("Page not found")
